8750d70ee7241087ee5a0641bab7fbb8badba06c
yuvrajmnit007 <yuvrajchoudhary3768@gmail.com>
2025-12-29 15:43:14 +0530

    cp 31 1100 rarted probelm no 1

--- B_Erase_First_or_Second_Letter.cpp
+++ B_Erase_First_or_Second_Letter.cpp
@@ -1,5 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string st;
+        cin>>st;
+        vector<int>freq(26,0);
+        int count=0;
+        int ans=1;
+        freq[st[0]-'a']=1;
+        count=1;
+        for(int i=1;i<n;i++){
+            freq[st[i]-'a']++;
+            if(freq[st[i]-'a']==1){
+                count++;
+            }
+            ans+=count;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
 }
