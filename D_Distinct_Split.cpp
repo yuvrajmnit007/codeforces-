37ea9e8226ba41621fc2dc9abcabbd75ec2bd74a
yuvrajmnit007 <yuvrajchoudhary3768@gmail.com>
2026-01-03 17:03:47 +0530

    1000 rated cp 31

--- D_Distinct_Split.cpp
+++ D_Distinct_Split.cpp
@@ -8,6 +8,29 @@ int main(){
         cin>>n;
         string st;
         cin>>st;
-        
+        vector<int>b(26,0);
+        vector<int>a(26,0);
+        int count=0;
+        for(int i=0;i<n;i++){
+            b[st[i]-'a']++;
+            if(b[st[i]-'a']==1){
+                count++;
+            }
+        }
+        int ans=0;
+        int count2=0;
+        for(int i=0;i<n;i++){
+            a[st[i]-'a']++;
+            b[st[i]-'a']--;
+            if(a[st[i]-'a']==1){
+                count2++;
+            }
+            if(b[st[i]-'a']==0){
+                    count--;
+            }
+            ans=max(ans,count+count2);
+        }
+        cout<<ans<<endl;
     }
+
 }
