bc493bb8f6125e76eb835e806427d10cbb8399d8
yuvrajmnit007 <yuvrajchoudhary3768@gmail.com>
2026-02-26 19:10:31 +0530

    26-02-26

--- B_Sum_of_Medians.cpp
+++ B_Sum_of_Medians.cpp
@@ -9,15 +9,14 @@ signed main() {
     while (t--) {
         int n,k;
         cin>>n>>k;
-        vector<int>arr(n*k+1);
-        arr[0]=0;
-        for(int i=1;i<=n*k;i++){
-            cin>>arr[i];
-        }
+        vector<int>arr(n*k);
+        for(int i=0;i<n*k;i++)cin>>arr[i];
         sort(arr.begin(),arr.end());
         int ans=0;
-        for(int i=k;i<=n*k;i=i+k-1){
-            ans+=arr[k];
+        int m=n*k;
+        while(k--){
+            m-=n/2+1;
+            ans+=arr[m];
         }
         cout<<ans<<endl;
     }
