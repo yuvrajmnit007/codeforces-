d735153a5b095d6c9e2f7e68dd418923df7dc363
yuvrajmnit007 <yuvrajchoudhary3768@gmail.com>
2026-04-15 19:04:00 +0530

    15-04-26

--- C_Find_B.cpp
+++ C_Find_B.cpp
@@ -1,35 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool solve(vector<int>nums,int l, int r){
-    int k=r-l+1;
-    unordered_map<int,int> mp;
-    int Val = nums[l];
-    int count = 0;
-    for(int i=l;i<=r;i++){
-        mp[nums[i]]++;
-        if(mp[nums[i]]>count){
-            count=mp[nums[i]];
-        }
-    }
-    if(count*2>k){
-        return false;
-    }
-    return true;
-}
-int main(){
+#define int long long
+signed main(){
     int t;
     cin>>t;
     while(t--){
         int n,q;
         cin>>n>>q;
         vector<int>arr(n+1);
+        vector<int>pre(n+1,0),help(n+1,0);
         for(int i=1;i<=n;i++){
             cin>>arr[i];
+            pre[i]=pre[i-1]+arr[i];
+            if(arr[i]==1){
+                help[i]=help[i-1]+1;
+            }else {
+                help[i]=help[i-1];
+            }
         }
         while(q--){
             int l,r;
             cin>>l>>r;
-            if(solve(arr,l,r)){
+            l--;
+            if(r-l+help[r]-help[l]<=pre[r]-pre[l]&&r-l>1){
                 cout<<"YES"<<endl;
             }else{
                 cout<<"NO"<<endl;
