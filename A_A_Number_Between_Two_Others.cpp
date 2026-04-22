69303081badf19f94420cade32bd9eb580c2c3b5
yuvrajmnit007 <yuvrajchoudhary3768@gmail.com>
2026-04-22 10:49:09 +0530

    22-04-26

---
+++ A_A_Number_Between_Two_Others.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int x,y;
+        cin>>x>>y;
+        int p=y/x;
+        if(p<=2){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+        }
+    }
+    return 0;
+}
