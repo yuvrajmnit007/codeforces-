fd26d8c393e2688dcb8e5d4f508c9f24bcb19483
yuvrajmnit007 <yuvrajchoudhary3768@gmail.com>
2026-05-07 22:05:39 +0530

    07-05-26

---
+++ B_Fun_with_Even_Subarrays.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                int sum=0;
+                for(int k=i;k<=j;k++){
+                    sum+=arr[k];
+                }
+                if(sum%2==0){
+                    ans++;
+                }
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
